190dd73078dca04aa860ab3dcf397d39ed52d7ad
Karlish <52657221+Karlish-git@users.noreply.github.com>
2025-01-03 14:36:03 +0200

    update lsp conf

--- nvim/.config/nvim/lua/custom/configs/lsp.lua
+++ nvim/.config/nvim/lua/custom/configs/lsp.lua
@@ -101,7 +101,7 @@ local servers = {
   jsonls = {},
   pyright = {},
   rust_analyzer = {},
-  ruff_lsp = {},
+  ruff = {},
   templ = {},
   typos_lsp = {},
   yamlls = {},
@@ -141,11 +141,19 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers {
   function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
+    local server = servers[server_name] or {}
+    -- This handles overriding only values explicitly passed
+    -- by the server configuration above. Useful when disabling
+    -- certain features of an LSP (for example, turning off formatting for ts_ls)
+    server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+    require('lspconfig')[server_name].setup(server)
   end,
+  -- function(server_name)
+  --   require('lspconfig')[server_name].setup {
+  --     capabilities = capabilities,
+  --     on_attach = on_attach,
+  --     settings = servers[server_name],
+  --     filetypes = (servers[server_name] or {}).filetypes,
+  --   }
+  -- end,
 }
