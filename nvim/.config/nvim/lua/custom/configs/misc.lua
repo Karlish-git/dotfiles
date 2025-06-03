dfac0ebb3e339f278647b02bbb7d9edc4aad4d09
Karlish <52657221+Karlish-git@users.noreply.github.com>
2025-06-03 17:49:30 +0300

    Add breakline clolours and local terminal open

--- nvim/.config/nvim/lua/custom/configs/misc.lua
+++ nvim/.config/nvim/lua/custom/configs/misc.lua
@@ -1,1 +1,9 @@
 vim.filetype.add({ extension = { templ = "templ" } })
+vim.api.nvim_create_user_command('LocalTerm', function()
+    local term_dir = vim.fn.expand('%:p:h')
+    vim.cmd('below new')
+    vim.fn.termopen({vim.o.shell}, {cwd = term_dir})
+    vim.cmd('startinsert')
+end, {})
+
+vim.keymap.set('n', '<leader>tt', ':LocalTerm<CR>', { noremap = true, silent = true, desc = 'Open terminal in current file directory' })
--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -1,7 +1,8 @@
-return{
-  {'romgrk/barbar.nvim',
+return {
+  {
+    'romgrk/barbar.nvim',
     dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
     init = function() vim.g.barbar_auto_setup = false end,
@@ -13,14 +14,14 @@ return{
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-},
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --     "MunifTanjim/nui.nvim",
+  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --   }
+  -- },
 }
